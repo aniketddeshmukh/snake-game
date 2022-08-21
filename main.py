d5bf32a65d5bf684d9d98ae9c6bf3d13e1495940
aniketddeshmukh <64788790+aniketddeshmukh@users.noreply.github.com>
2022-08-21 18:02:50 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+from turtle import Screen,Turtle
+import time
+from Snake import snake
+from food import *
+from Scoreboard import *
+
+screen = Screen()
+screen.setup(width=600,height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+Saanp = snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(Saanp.up,"Up")
+screen.onkey(Saanp.down,"Down")
+screen.onkey(Saanp.left,"Left")
+screen.onkey(Saanp.right,"Right")
+
+
+game_is_on =True
+while game_is_on:
+    Screen().update()
+    time.sleep(0.1)
+    Saanp.move()
+    #Collision with food
+    if Saanp.head.distance(food)<15:
+        food.refresh()
+        Saanp.extend()
+        scoreboard.increase_score()
+
+    #Collision with wall
+    if Saanp.head.xcor() >280 or Saanp.head.xcor() < -280 or Saanp.head.ycor() > 280 or Saanp.head.xcor() < -280:
+        game_is_on =False
+        scoreboard.game_over()
+    #Collision with  tail
+    for segment in Saanp.segments[1:]:
+        # if segment ==Saanp.head:
+        #     pass
+        if Saanp.head.distance(segment)<10:
+            game_is_on =False
+            scoreboard.game_over()
+
